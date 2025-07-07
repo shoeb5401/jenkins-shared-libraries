83e7d3370d7752a08196c7cb78036b87b1b36c93
Shoeb Qureshi <shoeb.qureshi.5401@gmail.com>
2025-07-07 12:41:56 +0530

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,4 +1,4 @@
-def cloneRepo(String url, String branch){
+def call(String url, String branch){
 	echo "Cloning the code from the Git"
 	git url: "${url}", branch:"${branch}"
 	echo 'Cloning is Successful'
